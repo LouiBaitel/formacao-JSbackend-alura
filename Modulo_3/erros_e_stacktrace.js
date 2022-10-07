3e600d946e4c32ebe78420b8dee2f890142f0fa2
Loui Junges <loui.junges@global.nn>
2022-10-07 14:30:09 -0300

    Adicional

--- Modulo_3/erros_e_stacktrace.js
+++ Modulo_3/erros_e_stacktrace.js
@@ -13,3 +13,15 @@ Esse tipo de erro ocorre quando nao declaramos a variável e tentamos utilizá-l
 /*
 Stacktrace: Lista mostrada no compilador quando temos algum erro. Dos arquivos internos também.
 */
+
+/*
+RangeError: Quando o código recebe um dado do tipo certo, porém não dentro do formato aceitável.
+*/
+
+/*
+TypeError: Indica que o código esperava receber um dado de um determinado tipo, tal qual uma string de texto, mas recebeu outro, como um número, booleano ou null.
+*/
+
+/*
+https://nodejs.org/api/errors.html#errors_errors
+*/
