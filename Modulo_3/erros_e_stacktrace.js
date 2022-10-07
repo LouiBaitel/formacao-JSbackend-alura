9a7eec49cfdc8e3e405aa49662dc24e9fb57522a
Loui Junges <loui.junges@global.nn>
2022-10-07 14:27:15 -0300

    Aula sobre erros

---
+++ Modulo_3/erros_e_stacktrace.js
@@ -0,0 +1,15 @@
+/*
+SyntaxError: Missing initializer in const declaration
+É um tipo de erro que acontece quando escrevemos o código com a sintaxe errada.
+*/
+//const numero;
+
+/*
+ReferenceError: minhaVar is not defined
+Esse tipo de erro ocorre quando nao declaramos a variável e tentamos utilizá-la.
+*/
+//console.log(minhaVar);
+
+/*
+Stacktrace: Lista mostrada no compilador quando temos algum erro. Dos arquivos internos também.
+*/
