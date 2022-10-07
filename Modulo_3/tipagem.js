74b702cf21af250be216e3497a9728ebb8185dd8
Loui Junges <loui.junges@global.nn>
2022-10-07 14:11:58 -0300

    Aula sobre tipagem

---
+++ Modulo_3/tipagem.js
@@ -0,0 +1,13 @@
+/*
+Tipagem dinâmica: O Js consegue identificar o tipo da variável sem que seja necessário informar o tipo da variável na declaração.
+Também pode ser chamada de linguagem Untyped;
+*/
+let minhaVar = 56;
+minhaVar = 'text';
+minhaVar = true;
+
+/*
+Linguagem multiparadigma: Conseguimos resolver o mesmo problema de diversar formas.
+ES6/ES2015: É a versão do Js que foi lançada em 2015. A partir dessa versão, passaram a lançar atualizações anuais.
+Linguagem interpretada: É uma linguagem que é lida por por um interpretador(navegador, nodeJS)
+*/
