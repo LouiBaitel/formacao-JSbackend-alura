6122269294b9c09e8431902b7b069181cdba613b
Loui Junges <loui.junges@global.nn>
2022-10-07 14:42:55 -0300

    Aula sobre console

---
+++ Modulo_3/cosole_api.js
@@ -0,0 +1,15 @@
+// O console.log() serve para exibir informações para o desenvolvedor, seja no compilador web ou node.js. Não aparece para o usuário.
+
+/*
+Tratamento de erro
+*/
+console.error('Erro!')
+
+//É uma boa prática utilizar um console.log() ou console.error() para sairmos da aplicação. Assim é possível acompanhar o funcionamento do código.
+
+/*
+console.table() para visualizar de forma mais organizada informações tabulares;
+console.time() e console.timeEnd() para temporizar período que uma operação de código leva para ser iniciada e concluída;
+console.trace() para exibir a stacktrace de todos os pontos (ou seja, os arquivos chamados) por onde o código executado passou durante a execução.
+https://nodejs.org/api/console.html
+*/
