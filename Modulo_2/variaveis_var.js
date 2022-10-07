25f864680d68bc1ba27096cd70826b57974bf2bc
Loui Junges <loui.junges@global.nn>
2022-10-07 11:20:34 -0300

    Subindo o módulo 2

---
+++ Modulo_2/variaveis_var.js
@@ -0,0 +1,37 @@
+//var: Pode ser declarada com um determinado valor, que pode ser substituido ao longo do código. Pode ser usada sem ser declarada.
+
+var altura = 5;
+var comprimento = 7;
+//É possível declarar a variável sem a var na frente pois o js entende que é uma var, mas isso não é uma boa prática.
+area = altura * comprimento;
+console.log('Teste com declaração var: '+ area);
+var area;
+
+//let: Tipo de variável que só pode ser usada após ser declarada;
+let forma = 'retangulo';
+let alturaL = 5;
+let comprimentoL = 7;
+let areaL;
+
+if (forma == 'retangulo') {
+    areaL = alturaL * comprimentoL;
+} else{
+    areaL = (alturaL * comprimentoL) / 2
+}
+
+console.log('Teste com declaração let: '+ areaL);
+
+//const
+
+const formaC = 'quadrado';
+const alturaC = 5;
+const comprimentoC = 7; 
+
+if (formaC == 'quadrado') {
+    areaC = alturaC * comprimentoC;
+} else{
+    areaC = (alturaC * comprimentoC) / 2;   
+}
+
+console.log('Teste com declaração const: '+ areaC);
+
