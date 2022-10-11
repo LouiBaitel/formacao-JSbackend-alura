0fe16c50f809d1dfe4eaa9cb3b57eb83f813fa92
Loui Junges <loui.junges@global.nn>
2022-10-11 15:22:22 -0300

    tipagens

---
+++ tipagens.js
@@ -0,0 +1,25 @@
+/*
+Tipos: Valores que são manipulados por um programa;
+    INT ou número inteiro: valores numéricos inteiros (positivos ou negativos);
+    FLOAT ou o chamado “ponto flutuante”: valores numéricos com casas após a vírgula (positivos ou negativos);
+    BOOLEAN ou booleanos: representado apenas por dois valores, “verdadeiro” e “falso”. Também chamados de operadores lógicos;
+    TEXT: sequências ou cadeias de caracteres, utilizados para manipular textos e/ou outros tipos de dados não numéricos ou booleanos, como hashes de criptografia.
+*/
+
+// ---------------- Linguagens estaticamente tipadas --------------------
+/*
+É o tipo de linguagem que precisa que seja informado qual o tipo do dado que está sendo tratado.
+A checagem (type checking) é feita na compilação do programa ou em tempo de execução (runtime), dependendo se a linguagem for compilada ou interpretada.
+Algumas linguagens estaticamente tipadas trabalham com a chamada inferência de tipo, técnica em que o programa consegue determinar sozinho o tipo da variável sem que seja necessário deixar essa informação explícita no código. 
+*/ 
+
+// ---------------- Linguagens dinamicamente tipadas --------------------
+/*
+O tipo é determinado (inferido) em runtime (ou tempo de execução) de acordo com o valor do dado, e não a partir da variável.
+*/
+
+
+/*
+Linguagens estaticamente tipadas têm uma performance de execução melhor, pois durante a execução o código já foi “traduzido” para linguagem máquina e a checagem/tipagem das variáveis já foi feito;
+As  linguagens dinamicamente tipadas costumam ser mais ágeis durante o desenvolvimento e também são mais flexíveis.
+*/
